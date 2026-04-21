81ffb7631e36c9e6855bed60c041ff959167f2e0
skeps <aliskepss@gmail.com>
2026-04-21 23:39:35 +0100

    Fixed syntax

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.api.routes.bmi import router as bmi_router
 
-app = FastAPI()
+app = FastAPI(title = "BMI API", description = "API for calculating Body Mass Index (BMI)", version = "1.0.0")
 
 
 @app.get("/")
@@ -9,4 +9,4 @@ def read_root():
     return {"message": "BMI API is running!"}
 
 
-app.include_router(bmi_router))
+app.include_router(bmi_router)
